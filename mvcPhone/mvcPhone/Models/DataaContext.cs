e4111fcff08f43fb8c4a581ee6204f245c746038
ValeriaBusch <valeritabusch2507@gmail.com>
2019-08-24 13:45:08 -0400

    Complete Solution

---
+++ mvcPhone/mvcPhone/Models/DataaContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace mvcPhone.Models
+{
+    public class DataaContext:DbContext
+    {
+        public DataaContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<mvcPhone.Models.Phone> Phones { get; set; }
+    }
+}
